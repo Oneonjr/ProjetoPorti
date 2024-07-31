de96097ecf94ccb691feea25d581135f6e2da19a
Oneonjr <oneonjunior@outlook.com>
2024-07-31 15:45:01 -0300

    Criação da navbar

---
+++ Home/scriptNavBar.js
@@ -0,0 +1,43 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks); // Correção para pegar todos os links
+        this.activeClass = "active";
+        
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        })
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass); // Adiciona a classe active na lista de navegação
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-links",
+    ".nav-links li"
+);
+
+mobileNavbar.init();
